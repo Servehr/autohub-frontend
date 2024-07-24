6d0c37e834b5505eb783cff7d3fc107ac4fef9fc
Hammed <servertstng@gmail.com>
2024-07-24 23:58:34 +0100

    Correcting Country Text 5

--- src/components/product/product-card.jsx
+++ src/components/product/product-card.jsx
@@ -170,8 +170,8 @@ export function ProductCard({ data, innerRef }) {
         <p className="text-brandRed leading-none">
           <IoLocationSharp size={10} className="inline" />{" "}
           <span className="text-xs md:text-sm text-red-600 font-semibold">
-            {theCountry + ", "}
-            {data?.state?.name}
+            {data?.country?.name + ", "}
+            {data?.state?.name} - {data?.country?.name}
           </span>
         </p>
       </div>
