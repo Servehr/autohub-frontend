9429cddc0b83670b9c0d9bed0438f1436ed7303e
Hammed <servertstng@gmail.com>
2024-09-11 01:08:23 +0100

    URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let token = localStorage.getItem("token")
 
-const DESTINATION = "http://127.0.0.1:8000/";
+const DESTINATION = "http://scentsbyhoppey.com/";
    
 
 export const BASE_URL = `${DESTINATION}api/app/`;
